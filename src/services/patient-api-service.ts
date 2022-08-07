b41770961e460fb882d646ddc173188251df8f02
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-08-07 09:44:52 +0300

    app service function to the backend

---
+++ src/services/patient-api-service.ts
@@ -0,0 +1,79 @@
+import { getStorageItem } from '../helpers/utils'
+
+// define types of the api
+interface Patient {
+  name: string | undefined
+  email: string | undefined
+  tel: string | undefined
+  DOB: Date | undefined
+  sex: string | undefined
+  region: string | undefined
+  city: string | undefined
+}
+
+interface EditPatient {
+  id: string
+  patient: Patient
+}
+// define headers
+
+const user = getStorageItem('user')
+const token = `Bearer ${user.item.Authorization}`
+console.log('token', token)
+
+const headers = new Headers({
+  Authorization: token,
+  'Content-Type': 'application/json',
+})
+
+// post a patient
+export const postPatient = async (patient: Patient) => {
+  const response = await fetch(`${process.env.REACT_APP_BASE_URI}/patient`, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(patient),
+  })
+  return response
+}
+// get all patients
+export const getAllPatients = async () => {
+  const response = await fetch(`${process.env.REACT_APP_BASE_URI}/patient`, {
+    method: 'GET',
+    headers: headers,
+  })
+  return await response.json()
+}
+// get a single patient
+export const getAPatient = async (id: string) => {
+  const response = await fetch(
+    `${process.env.REACT_APP_BASE_URI}/patient/${id}`,
+    {
+      method: 'GET',
+      headers: headers,
+    }
+  )
+  return await response.json()
+}
+// update a patient
+export const updatePatient = async ({ id, patient }: EditPatient) => {
+  const response = await fetch(
+    `${process.env.REACT_APP_BASE_URI}/patient/${id}`,
+    {
+      method: 'PUT',
+      headers: headers,
+      body: JSON.stringify(patient),
+    }
+  )
+  return response
+}
+// delete a patient TODO later
+export const deletePatient = async (id: string) => {
+  const response = await fetch(
+    `${process.env.REACT_APP_BASE_URI}/patient/${id}`,
+    {
+      method: 'DELETE',
+      headers: headers,
+    }
+  )
+  return response
+}
